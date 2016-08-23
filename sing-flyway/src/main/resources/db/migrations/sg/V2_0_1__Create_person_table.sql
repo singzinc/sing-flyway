d505056ac2b1ac5b68e91aa47eb670e39e2d8c5d
sing <sing.zinc.hk@gmail.com>
2016-08-23 18:44:50 +0800

    first version

---
+++ sing-flyway/src/main/resources/db/migrations/sg/V2_0_1__Create_person_table.sql
@@ -0,0 +1,23 @@
+
+USE [TestFlyWay]
+GO
+
+/****** Object:  Table [dbo].[testTable]    Script Date: 08/18/2016 12:10:34 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[testTable4](
+	[test_id] [int] NULL,
+	[txt_content] [varchar](255) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
